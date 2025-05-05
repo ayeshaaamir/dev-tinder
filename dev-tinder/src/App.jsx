0c96ee2e42418508765c3a0913ffd59b6cac8eac
ayeshaaamir <ayeshaaamir38@gmail.com>
2025-05-05 17:25:45 +0100

    daisy UI installed..

--- dev-tinder/src/App.jsx
+++ dev-tinder/src/App.jsx
@@ -1,3 +1,15 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-base-200">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">👋 Hello, Developer!</h2>
+          <p>Welcome to DevTinder using Tailwind + daisyUI!</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
